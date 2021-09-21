552508bf38781edf03211d020b6b6c2f147b14c5
fprebolledo <fprebolledo@uc.cl>
2021-09-20 22:28:42 -0300

    feat(main): change main

--- lib/main.rb
+++ lib/main.rb
@@ -1,13 +1,6 @@
 # frozen_string_literal: true
-require 'matrix'
 
-require_relative './board_model'
-require_relative './board_controller'
-require_relative './board_view'
+require_relative './cell_model'
 
-model = Board.new()
-view = BoardView.new()
-model.addObserver(view)
-controller = BoardController.new(model,view)
-controller.printBoard
-controller.requestInput
+cell = Cell.new(1, false)
+puts cell.value
