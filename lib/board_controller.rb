14c40329f6e98f561a61932e88cc0c33bd89e982
egromero <egromero@uc.cl>
2021-09-28 11:07:29 -0300

    fix(controller): fix rubocop

--- lib/board_controller.rb
+++ lib/board_controller.rb
@@ -1,30 +1,35 @@
 # frozen_string_literal: true
-INSTRUCTIONS = "Ingresa las coordenadas de la celda a descubrir\nEl formato es separados por coma Ejemplo: 2,1\nPara terminar ingresa 'e'"
+INSTRUCTIONS = "Ingresa las coordenadas de la celda a descubrir
+El formato es separados por coma Ejemplo: 2,1
+Para terminar ingresa 'e'"
+
 ERROR_INPUT = 'Formato de coordenada incorrecto. ej: 2,1'
 
+# boar controller.
 class BoardController
-    def getUserInput
-        correct_input = false
-        puts INSTRUCTIONS
-        while !correct_input
-            input = gets.chomp
-            return -1 if input == 'e'
-            x,y  = input.split(',')
-            correct_input = (isCorrect?(x) and isCorrect?(y))
-            puts ERROR_INPUT if !correct_input
-        end
-        return x,y
-    end
+  def request_user_input
+    correct_input = false
+    puts INSTRUCTIONS
+    until correct_input
+      input = gets.chomp
+      break if input == 'e'
 
-    private 
-    def isCorrect?(input)
-        return false if input.nil? or input.empty?
-        return false if !input.match("^[0-9]$")
-        return false if input.to_i > 8 
-        return true
+      x, y = input.split(',')
+      correct_input = (correct?(x) and correct?(y))
+      puts ERROR_INPUT unless correct_input
     end
+  end
+
+  private
+
+  def correct?(input)
+    return false if input.nil? || input.empty?
+    return false unless input.match('^[0-9]$')
+    return false if input.to_i > 8
+    return true
+  end
 end
 
 
-b = BoardController.new
-puts b.getUserInput
+a = BoardController.new
+a.request_user_input
