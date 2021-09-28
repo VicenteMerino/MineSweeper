cf7909c9a00c1fdfa9c10af951acb2e2b13a88c3
fprebolledo <fprebolledo@uc.cl>
2021-09-27 21:56:54 -0300

    fix(): separte tests by context

--- test/board_test.rb
+++ test/board_test.rb
@@ -17,41 +17,6 @@ class BoardTest < Test::Unit::TestCase
                    [nil, nil, nil, Cell.new(9, false), nil]]
   end
 
-  def test_random_board_size
-    board_object = Board.new
-    board_matrix = board_object.board
-    size = board_object.size
-    height = board_matrix.length
-    width = board_matrix[0].length
-    assert_equal(size, height)
-    assert_equal(size, width)
-  end
-
-  def test_random_board_bombs
-    board_object = Board.new
-    board_matrix = board_object.board
-    bombs = board_object.bombs
-    bombs_count = 0
-    board_matrix.each do |row|
-      row.each do |cell|
-        bombs_count += 1 if cell.value == 9
-      end
-    end
-    assert_equal(bombs, bombs_count)
-  end
-
-  def test_nil_cells
-    board_object = Board.new
-    board_matrix = board_object.board
-    nil_cells = 0
-    board_matrix.each do |row|
-      row.each do |cell|
-        nil_cells += 1 if cell.nil?
-      end
-    end
-    assert_equal(0, nil_cells)
-  end
-
   def test_select_with_neighbor
     board_with_neighbor = Board.new(board_values: [[[0, true], [0, true], [0, true], [1, true], [9, false]],
                                                    [[0, true], [0, true], [1, true], [2, true], [2, false]],
@@ -89,15 +54,6 @@ class BoardTest < Test::Unit::TestCase
     board_test = Board.new(board_values: values)
     assert_equal(board_test.board.length, values.length)
     assert_equal(board_test.board[0].length, values[0].length)
-  end
-
-  def test_board_values
-    filled_board = @board.assign_board_cells(@bomb_board)
-    (0..filled_board.length - 1).each do |row_index|
-      (0..filled_board[0].length - 1).each do |col_index|
-        assert_equal(filled_board[row_index][col_index]&.value, @board.board[row_index][col_index].value)
-      end
-    end
   end
 
   def test_board_test_bombs
