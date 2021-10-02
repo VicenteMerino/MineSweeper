43cd171a8ed399ce3ec2bdea6e98f9dacb9074d1
Seld0nn <37000553+Seld0nn@users.noreply.github.com>
2021-10-02 11:09:51 -0300

    Flujo del juego
    4. descrubir celda
    6. revisar si gano
    7 print de tablero

--- lib/game_flow.rb
+++ lib/game_flow.rb
@@ -8,12 +8,15 @@ require 'colorize'
 def rungame(board, view)
   puts "\n Bienvenido a Minesweeper. \n Su tablero se presenta a continuacion: \n"
   view.print_board(board)
-
-  puts "\n Ingrese la fila en que desea jugar: "
-  selectedRow = gets.chomp.to_i
-  puts "\n Ingrese la Columna en que desea jugar: "
-  selectedCol = gets.chomp.to_i
-  puts "Selecciono la coordenada ("+ selectedRow.to_s+","+selectedCol.to_s+ ") -> Actualizando tablero: \n"
-
+  while board.check_victory == false
+    puts "\n Ingrese la fila en que desea jugar: "
+    selectedRow = gets.chomp.to_i
+    puts "\n Ingrese la Columna en que desea jugar: "
+    selectedCol = gets.chomp.to_i
+    puts "Selecciono la coordenada ("+ selectedRow.to_s+","+selectedCol.to_s+ ") -> Actualizando tablero: \n"
+    board.undercover_cell(selectedRow, selectedCol)
+    view.update(board)
+  end  
+  view.congratulate
 
 end
